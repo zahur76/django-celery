88d9fe609cd6a773831725e771c7b24489970b63
zahur <zahurmeerun@hotmail.com>
2022-06-17 13:26:26 +0400

    add: celery task to view

--- home/views.py
+++ home/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from .tasks import debug_task
 
 # Create your views here.
 def index(request):
     """A view to return the home page"""
+
+    debug_task.delay()
 
     context = {
     }
